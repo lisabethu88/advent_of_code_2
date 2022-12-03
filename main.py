17cc62f4a4a52399905c06978823d9beeb123416
Lisa Utsett <lisabethutsett@gmail.com>
2022-12-02 19:25:50 -0500

    Finished part 2

--- main.py
+++ main.py
@@ -6,6 +6,31 @@ WIN = 6
 TIE = 3
 TIED_SCENARIOS = ["C Z", "A X", "B Y"]
 WINNING_SCENARIOS = ["B Z", "A Y", "C X"]
+"""A for Rock, B for Paper, and C for Scissors"""
+def lose(their_choice):
+    if their_choice == "A":
+        return SCISSORS
+    elif their_choice == 'B':
+        return ROCK
+    else:
+        return PAPER
+    
+
+def win(their_choice):
+    if their_choice == "A":
+        return PAPER
+    elif their_choice == 'B':
+        return SCISSORS
+    else:
+        return ROCK
+
+def tie(their_choice):
+    if their_choice == "A":
+        return ROCK
+    elif their_choice == 'B':
+        return PAPER
+    else:
+        return SCISSORS
 
 def update_score(line):
     if line[2]== 'X':
@@ -15,17 +40,15 @@ def update_score(line):
     elif line[2] == 'Z':
         return SCISSORS
 
-def get_winner(line):
-    if line in TIED_SCENARIOS:
-        return TIE
-    elif line in WINNING_SCENARIOS:
-        return WIN
-    else:
-        return LOSE
 
-def get_choice(my_move, their_choice):
-    if my_move == 'X'
-    pass
+def get_score(my_move, their_choice):
+    if my_move == 'X': 
+        return LOSE + lose(their_choice)
+    elif my_move == "Y":
+        return TIE + tie(their_choice)
+    else:
+        return WIN + win(their_choice)
+    
 
 my_score = 0
 
@@ -33,12 +56,8 @@ f = open("input.txt","r")
 for line in f:
     their_choice = line[0]
     my_move = line[2]
-    my_choice = get_choice(my_move, their_choice)
-    line = line.strip()
-    my_score += update_score(line)
-    my_score += get_winner(line)
+    my_score += get_score(my_move, their_choice)
     
 
 f.close()
 print(my_score)
-"""x = lose, y = draw, z = win"""
